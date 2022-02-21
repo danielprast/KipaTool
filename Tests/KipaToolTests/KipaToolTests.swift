556acfb3d2a097c0afda004c590139b21a4e782b
Daniel Prastiwa <daniel.prastiwa@akupeduli.org>
2022-02-21 14:37:50 +0700

    Adding platform & clean up code

--- Tests/KipaToolTests/KipaToolTests.swift
+++ Tests/KipaToolTests/KipaToolTests.swift
@@ -6,6 +6,6 @@ final class KipaToolTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(KipaTool().text, "Hello, World!")
+        //XCTAssertEqual(KipaTool().text, "Hello, World!")
     }
 }
